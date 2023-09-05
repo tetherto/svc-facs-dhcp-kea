155c6d97a3c8f1a5cef2bc2b0f8b9a6efd4f2a65
Subash <sns@vuln.in>
2023-09-05 15:09:38 +0000

    getLeases working

--- index.js
+++ index.js
@@ -9,38 +9,54 @@ class KEAFacility extends BaseFacility {
     super(caller, opts, ctx)
     this.name = 'kea'
     this._hasConf = true
-    this.init()
+    super.init()
   }
 
-  async keaCall (command, service) {
-    body = {
+  sendCommand (command, service, args = undefined ) {
+    let body = {
       command,
       service
+    }
+    if(args)
+    {
+      body.arguments = args
     }
     return axios.post(this.conf.url, body)
   }
 
-  async getAll(){
+  async _lease4GetAll(){
     try {
-      const res = await this.keaCall
-      console.log(res);
+      const res = await this.sendCommand("lease4-get-all",["dhcp4"])
+      return res.data[0].arguments.leases
     } catch (error) {
       console.error(error);
     }
   }
 
-
-  _stop (cb) {
-    async.series([
-      next => { super._stop(next) },
-      async () => {
-        if (this.server) {
-          await this.server.close()
-          aedes.close()
-        }
-      }
-    ], cb)
+  async _lease4set(){
+    try {
+      const res = await this.sendCommand("lease4-get-all",["dhcp4"])
+      return res.data[0].arguments.leases
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  async getLeases(){
+    const res = await this._lease4GetAll()
+    return res.map((val) => ({"mac": val["hw-address"], "ip": val["ip-address"]}))
+  }
+
+  async setLeases(leases){
+    //take array of ip,mac objects and set, error on conflict, etc.
+    //figure if we need to hot reload the server or automatic, etc.
+  }
+
+  async freeLeases(leases){
+    //take array of ip,mac objects and free, error on conflict, etc.
+    //figure if we need to hot reload the server or automatic, etc.
+  }
+
 }
 
 module.exports = KEAFacility
